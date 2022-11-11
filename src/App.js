d8dd99c0436c66ac4cc7ef2602ccd4b58129bd0f
Yadav-Devendra <yadavdevendra476@gmail.com>
2022-11-11 22:17:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState} from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([])
+  return (
+    <>
+    <TodoList todos={todos}/>
+    <imput type="text" />
+    <button>Add Todo</button>
+    <button>Clear Complete</button>
+    <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
